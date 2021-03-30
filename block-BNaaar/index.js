187cf3fd782ca27af5327e6a5ccb8aab7073a993
artikgupta <artig9262@gmail.com>
2021-03-30 22:01:09 +0530

    block 2 of handling multiple request

--- block-BNaaar/index.js
+++ block-BNaaar/index.js
@@ -8,11 +8,15 @@ function handleRequest(req, res) {
   //   res.end("<h2>Welcome</h2>");
 
   if (req.method === "GET" && req.url === "/") {
+    res.setHeader("Content-Type", "text/plain");
     res.write("Welcome to homepage");
     res.end();
   } else if (req.method === "GET" && req.url === "/about") {
     res.setHeader("Content-Type", "text/html");
-    res.end(<h2>this is all about NodeJS</h2>);
+    res.end("<h2>this is all about NodeJS</h2>");
+  } else if (req.method === "POST" && req.url === "/about") {
+    res.setHeader("Content-Type", "application/json");
+    res.end("{message: this is a post request}");
   }
 }
 
